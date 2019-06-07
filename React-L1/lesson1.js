bf69dd5bed205169ef4a8cef5c4c87146c01080b
Artsiom Drushchyts <temafm-3@yandex.ru>
2019-06-07 22:12:18 +0300

    add description task

---
+++ React-L1/lesson1.js
@@ -0,0 +1,12 @@
+const employers = ['Alex', '', 'ludmila', 'Viktor', '', 'oleg', 'iNna', 'Ivan', 'Alex', 'Olga', ' Ann'];
+
+const employersNames = employers
+    .filter(name => name.length > 0)
+    .map(name => name.toLowerCase().trim())
+
+const sponsors = {
+    cash: [40000, 5000, 30400, 12000],
+    eu: ['SRL', 'PLO', 'J&K'],
+    rus: ['RusAuto', 'SBO']
+};
+console.log(employersNames);
